e577729c35dec4fa0da0f9ff37887cee5bfca95c
ThibaharS <P16208730@my365.dmu.ac.uk>
2018-10-22 14:34:33 +0100

    my first commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+<!-- Returns Hello as a pop up box-->
+<!-- alert("Hello!"); -->
+
+<!-- Loads hello within console log -->
+<!-- console.log("Hello!"); -->
+
+
+
+const hello = document.getElementById("Hello");
+
+<!--Function -->
+
+function greeting() {
+  console.log("Hello!");
+  alert("Hello!");
+}
+
+hello.addEventListener("click", greeting);
